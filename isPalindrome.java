477ec7f46b9c2ba6b8628064ee56f6a713c7a9c4
Yangtaitai <amyfan@gwmail.gwu.edu>
2016-02-09 15:31:42 -0500

    Update isPalindrome.java

--- isPalindrome.java
+++ isPalindrome.java
@@ -26,3 +26,41 @@
     }
     return true;
  }
+ 
+ // the second approach is reverse half of the list and compare two sublists, which is O(1) space
+  public boolean isPalindrome(ListNode head) {
+    if(head == null || head.next == null) return true;
+    
+    ListNode fast = head;
+    ListNode slow = head;
+    
+    //use fast to find the middle of the list
+    while(fast.next != null && fast.next.next != null){
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    
+    //get the second part of the list
+    ListNode secondHead = slow.next;
+    slow.next = null;
+    
+    ListNode p1 = secondHead;
+    ListNode p2 = p1.next;
+    secondHead.next = null;
+    
+    while(p1 != null && p2 != null){
+      ListNode temp = p2.next;
+      p2.next = p1;
+      p1 = p2;
+      p2 = temp;
+    }
+    
+    ListNode newPre = p1;
+    ListNode newHead = head;
+    while(newPre != null){
+       if(newPre.val != newHead.val){ reture false;}
+       newPre = newPre.next;
+       newHead = newHead.next;
+    }
+    return true;
+  }
