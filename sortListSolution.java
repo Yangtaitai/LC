06fdc41586416c9a5d855fbf8515d041659abd0b
Yangtaitai <amyfan@gwmail.gwu.edu>
2016-02-23 17:21:03 -0500

    Update sortListSolution.java

--- sortListSolution.java
+++ sortListSolution.java
@@ -12,6 +12,7 @@
  
 // time comlexity require we can use quick sort or merge sort for linked list
 
+// mergeSort way of sort list
 public class sortListSolution{
     public ListNode sortList{
       if(head == null && head.next == null) return head;
@@ -61,3 +62,70 @@ public class sortListSolution{
     return dummy.next;
   }
 }
+
+//quickSort way of sort list, the partition done a lot work similar to the merge part of the mergeSort way.
+public class sortListSolution{
+  public ListNode sortList(ListNode head){
+    if(head == null || head.next == null) return head;
+    ListNode mid = getMiddle(head);
+    
+    ListNode leftDummy = new ListNode(0), leftTail = leftDummy;
+    ListNode middleDummy = new ListNode(0), middleTail = middleDummy;
+    ListNode rightDummy = new ListNode(0), rightTail = rightDummy;
+    
+    while(head != null){
+     if(head.val < mid.val){
+      leftTail.next = head;
+      head = head.next;
+     }else if(head.val > mid.val){
+      rightTail.next = head;
+      head = head.next;
+     }
+     middleTail.next = head;
+     head = head.next;
+    }
+    leftTail = null;
+    middleTail = null;
+    rightTail = null;
+    
+    ListNode left = sortList(leftDummy.next);
+    ListNode right = sortList(rightDummy.next);
+    
+    return concat(left, middleDummy.next,right);
+  }
+  
+  // get the middle of the linked list
+  public ListNode getMiddle(ListNode head){
+   if(head == null || head.next == null) return head;
+   ListNode slow = head;
+   ListNode fast = head.next;
+   while(fast != null && fast.next != null){
+    slow = slow.next;
+    fast = fast.next.next;
+   }
+   return slow;
+  }
+  
+  // concat the partitioned lists
+  public ListNode concat(ListNode left,ListNode middle, ListNode right){
+   ListNode dummy = new ListNode(0), tail = dummy;
+   tail.next = left;
+   tail = getTail(tail);
+   tail.next = middle;
+   tail = getTail(tail);
+   tail.next = right;
+   tail = getTail(tail);
+   return dummy.next;
+  }
+  
+  //get the tail of the linked list for the concat part
+  public ListNode getTail(ListNode head){
+   if(head == null) return head;
+   while(head.next != null){
+    head = head.next;
+   }
+   return head;
+  }
+  
+}
+
