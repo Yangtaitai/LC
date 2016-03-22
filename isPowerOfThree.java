47e8515c47be7b8f76cf8d07798cd92e3a315fc1
Yangtaitai <amyfan@gwmail.gwu.edu>
2016-03-22 19:22:58 -0400

    Update isPowerOfThree.java

--- isPowerOfThree.java
+++ isPowerOfThree.java
@@ -11,3 +11,19 @@ public boolean isPowerOfThree(int n){
   else if(n % 3 == 0) return isPowerOfThree(n/3);
   else return false;
 }
+
+// approach #2 loop way
+public boolean isPowerOfThree(int n){
+  if(n<0 || n==0) return false;
+  while(n%3==0){
+   n=n/3;
+  }
+  return n==1;
+}
+
+// approach #3 without loop or recursion, find the largest number that is power of three and all the left one can be the divisor.
+public boolean isPowerOfThree(int n){
+  if(n<0 || n==0) return false;
+  int maxNumber = Math.pow(3,(int)(Math.log(0x7fffffff)/Math.log(3)));
+  return maxNumber%n == 0;
+}
