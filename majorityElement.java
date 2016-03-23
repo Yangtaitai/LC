b99aecf6a67b958332bd0bb28b8068c9361fadaf
Yangtaitai <amyfan@gwmail.gwu.edu>
2016-03-23 15:20:23 -0400

    Majority element

---
+++ majorityElement.java
@@ -0,0 +1,10 @@
+/** Given an array of size n, find the majority element. 
+  * The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+  * You may assume that the array is non-empty and the majority element always exist in the array.
+  */
+
+ public int majorityElement(int[] nums) {
+     if(nums.length == 1) return int[0];
+     Arrays.sort(nums);
+     return nums[nums.length/2];
+ }
