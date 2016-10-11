dc4fdebcd92195c61de6ef20b7f45cbb324337d9
Yangtaitai <amyfan@gwmail.gwu.edu>
2016-10-11 19:48:35 -0400

    Create HappyNumber.js

---
+++ HappyNumber.js
@@ -0,0 +1,24 @@
+// Write an algorithm to determine if a number is "happy".
+// A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+// Example: 19 is a happy number
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    if(n < 10){
+        if(n === 1 || n === 7) return true;
+        else return false;
+    }
+    var temp = n.toString();
+    var sum = 0;
+    for(var i = 0; i < temp.length; i++){
+        sum += parseInt(temp[i]) * parseInt(temp[i]);
+    }
+    return isHappy(sum);
+}
